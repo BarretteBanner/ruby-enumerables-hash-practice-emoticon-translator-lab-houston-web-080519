df335c959c25e8b89bf33e8b2a432d5609403ec8
Barrette Banner <barrettebanner@gmail.com>
2019-07-18 23:16:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
   # code goes here
   library = load_library(file_path)
-  if library[get_emoticon]? (emoticon)
+  if library["get_emoticon"]? (emoticon)
     library['get_emoticon'][emoticon]
   else
     "Sorry, that emoticon was not found."
