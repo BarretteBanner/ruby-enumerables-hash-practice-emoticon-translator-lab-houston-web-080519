f3aed9cf75a358f2c36157e5f2a7fe7acb22b47f
Barrette Banner <barrettebanner@gmail.com>
2019-07-18 23:05:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,18 +11,8 @@ end
 result
 end 
 
-=begin def load_library(file_path)
-  library = YAML.load_file(file_path)
-  result = {"get_meaning" => {}, "get_emoticon" => {}}
-  library.each do |meaning, emoticons|
-    result["get_meaning"][emoticons[1]] = meaning
-    result["get_emoticon"][emoticons[0]] = emoticons[1]
-  end
-  result
-end
-=end 
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
 end
 
