7316019d4befdd61cc3d8feb011d8ad53b76c884
Barrette Banner <barrettebanner@gmail.com>
2019-07-18 23:20:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ end
 
 def get_english_meaning(file_path, emoticon)
   library = load_library(file_path)
-  if library["get_meaning"].include(emoticon)
+  if library["get_meaning"].include?(emoticon)
     library["get_meaning"][emoticon]
   else 
     "Sorry, that emoticon was not found"
