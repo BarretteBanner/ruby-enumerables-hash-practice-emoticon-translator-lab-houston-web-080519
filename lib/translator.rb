fa9baa49ff4ba521660a123661c4ad9b1b9b5e2d
Barrette Banner <barrettebanner@gmail.com>
2019-07-18 23:14:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
   # code goes here
   library = load_library(file_path)
   if library[get_emoticon]? (emoticon)
-    
+    library['get_emoticon']['emoticon']
   
 end
 
